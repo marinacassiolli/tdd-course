5d35af48409f33e86d16ff44324800fde52b52a3
Marina Teixeira <marina@vizir.com.br>
2017-10-28 11:27:25 -0200

    Improve calculator spec

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -3,10 +3,24 @@ require 'calculator'
 
 # describe: descreve o que vai ser testado, pode ser texto ou classe
 describe Calculador do
-  # it/specify/example: demonstra qual cenário vai ser testado
-  it 'sum two numbers' do
-    calculator = Calculador.new
-    result = calculator.sum(18, 6)
-    expect(result).to eq(24)
+  context 'when #sum' do
+    # it/specify/example: demonstra qual cenário vai ser testado
+    it 'with positive numbers' do
+      calculator = Calculador.new
+      result = calculator.sum(18, 6)
+      expect(result).to eq(24)
+    end
+
+    it 'with negative numbers' do
+      calculator = Calculador.new
+      result = calculator.sum(-10, -5)
+      expect(result).to eq(-15)
+    end
+
+    it 'with negative and negative numbers' do
+      calculator = Calculador.new
+      result = calculator.sum(-5, 7)
+      expect(result).to eq(2)
+    end
   end
 end
