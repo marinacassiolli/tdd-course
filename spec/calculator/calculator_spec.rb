f2ae3e5579f3a3f94e4433417f59fb6bd298fe0e
Marina Teixeira <marina@vizir.com.br>
2017-10-30 22:44:52 -0200

    Change to implicit subject

--- spec/calculator/calculator_spec.rb
+++ spec/calculator/calculator_spec.rb
@@ -3,23 +3,24 @@ require 'calculator'
 
 # describe: descreve o que vai ser testado, pode ser texto ou classe
 describe Calculador do
-  # subject: instância de um objeto utilizada em todo teste
-  subject(:calculator) { described_class.new() }
+  # subject explícito: subject sobreescrito
+  # subject(:calculator) { described_class.new() }
 
+  # subject implícito: no nosso caso, a classe Calculator, não precisa ser instânciada
   context 'when #sum' do
     # it/specify/example: demonstra qual cenário vai ser testado
     it 'with positive numbers' do
-      result = calculator.sum(18, 6)
+      result = subject.sum(18, 6)
       expect(result).to eq(24)
     end
 
     it 'with negative numbers' do
-      result = calculator.sum(-10, -5)
+      result = subject.sum(-10, -5)
       expect(result).to eq(-15)
     end
 
     it 'with negative and negative numbers' do
-      result = calculator.sum(-5, 7)
+      result = subject.sum(-5, 7)
       expect(result).to eq(2)
     end
   end
